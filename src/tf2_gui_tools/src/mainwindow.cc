3b5ce7d605cdd4fb82cbd7ca1b1eafad45452342
HuaTsai <huatsai.eed07g@nctu.edu.tw>
2024-06-05 16:31:43 +0800

    fix: smooth interface

--- src/tf2_gui_tools/src/mainwindow.cc
+++ src/tf2_gui_tools/src/mainwindow.cc
@@ -10,44 +10,51 @@
 #endif
 
 namespace {
-// XYZ: -2.00(0):0.01:2.00(400)
+
+constexpr int kScale = 1000;
+constexpr double kXYZMax = 2.0;
+constexpr double kXYZMin = -2.0;
+constexpr double kRPYMax = 100.0;
+constexpr double kRPYMin = -100.0;
+
 void InitXYZ(QSlider *slider, QLineEdit *le) {
-  slider->setMinimum(0);
-  slider->setMaximum(400);
-  slider->setValue(200);
+  slider->setMinimum(kXYZMin * kScale);
+  slider->setMaximum(kXYZMax * kScale);
+  slider->setValue(0);
   le->setText("0");
+  le->setValidator(new QDoubleValidator(kRPYMin, kRPYMax, 3));
 }
 
 std::optional<double> SliderToValueXYZ(int tick) {
-  if (tick > 400 || tick < 0)
+  if (tick > kXYZMax * kScale || tick < kXYZMin * kScale)
     return std::nullopt;
-  return (tick - 200) * 0.01;
+  return static_cast<double>(tick) / kScale;
 }
 
 std::optional<int> ValueToSliderXYZ(double value) {
-  if (value > 2.0 || value < -2.0)
+  if (value > kXYZMax || value < kXYZMin)
     return std::nullopt;
-  return static_cast<int>(value * 100) + 200;
+  return value * kScale;
 }
 
-// RPY: -90(0):0.5:90(360)
 void InitRPY(QSlider *slider, QLineEdit *le) {
-  slider->setMinimum(0);
-  slider->setMaximum(360);
-  slider->setValue(180);
+  slider->setMinimum(kRPYMin * kScale);
+  slider->setMaximum(kRPYMax * kScale);
+  slider->setValue(0);
   le->setText("0");
+  le->setValidator(new QDoubleValidator(kRPYMin, kRPYMax, 3));
 }
 
 std::optional<double> SliderToValueRPY(int tick) {
-  if (tick > 360 || tick < 0)
+  if (tick > kRPYMax * kScale || tick < kRPYMin * kScale)
     return std::nullopt;
-  return (tick - 180) * 0.5;
+  return static_cast<double>(tick) / kScale;
 }
 
 std::optional<int> ValueToSliderRPY(double value) {
-  if (value > 90. || value < -90.)
+  if (value > kRPYMax || value < kRPYMin)
     return std::nullopt;
-  return static_cast<int>(value * 100) + 180;
+  return value * kScale;
 }
 } // namespace
 
@@ -69,16 +76,16 @@ MainWindow::MainWindow(int id, QWidget *parent)
   connect(ui->slider_x, &QSlider::valueChanged, this, [this]() { UpdateSliderXYZ(ui->slider_x, ui->le_x); });
   connect(ui->slider_y, &QSlider::valueChanged, this, [this]() { UpdateSliderXYZ(ui->slider_y, ui->le_y); });
   connect(ui->slider_z, &QSlider::valueChanged, this, [this]() { UpdateSliderXYZ(ui->slider_z, ui->le_z); });
-  connect(ui->le_x, &QLineEdit::editingFinished, this, [this]() { UpdateLineEditXYZ(ui->le_x, ui->slider_x); });
-  connect(ui->le_x, &QLineEdit::editingFinished, this, [this]() { UpdateLineEditXYZ(ui->le_y, ui->slider_y); });
-  connect(ui->le_z, &QLineEdit::editingFinished, this, [this]() { UpdateLineEditXYZ(ui->le_z, ui->slider_z); });
+  connect(ui->le_x, &QLineEdit::textChanged, this, [this]() { UpdateLineEditXYZ(ui->le_x, ui->slider_x); });
+  connect(ui->le_y, &QLineEdit::textChanged, this, [this]() { UpdateLineEditXYZ(ui->le_y, ui->slider_y); });
+  connect(ui->le_z, &QLineEdit::textChanged, this, [this]() { UpdateLineEditXYZ(ui->le_z, ui->slider_z); });
 
   connect(ui->slider_rx, &QSlider::valueChanged, this, [this]() { UpdateSliderRPY(ui->slider_rx, ui->le_rx); });
   connect(ui->slider_ry, &QSlider::valueChanged, this, [this]() { UpdateSliderRPY(ui->slider_ry, ui->le_ry); });
   connect(ui->slider_rz, &QSlider::valueChanged, this, [this]() { UpdateSliderRPY(ui->slider_rz, ui->le_rz); });
-  connect(ui->le_rx, &QLineEdit::editingFinished, this, [this]() { UpdateLineEditRPY(ui->le_rx, ui->slider_rx); });
-  connect(ui->le_rx, &QLineEdit::editingFinished, this, [this]() { UpdateLineEditRPY(ui->le_ry, ui->slider_ry); });
-  connect(ui->le_rz, &QLineEdit::editingFinished, this, [this]() { UpdateLineEditRPY(ui->le_rz, ui->slider_rz); });
+  connect(ui->le_rx, &QLineEdit::textChanged, this, [this]() { UpdateLineEditRPY(ui->le_rx, ui->slider_rx); });
+  connect(ui->le_ry, &QLineEdit::textChanged, this, [this]() { UpdateLineEditRPY(ui->le_ry, ui->slider_ry); });
+  connect(ui->le_rz, &QLineEdit::textChanged, this, [this]() { UpdateLineEditRPY(ui->le_rz, ui->slider_rz); });
 
   connect(ui->btn_sendtf, &QPushButton::clicked, this, &MainWindow::SendTF);
   connect(ui->btn_sendstf, &QPushButton::clicked, this, &MainWindow::SendStaticTF);
@@ -93,11 +100,12 @@ MainWindow::MainWindow(int id, QWidget *parent)
 MainWindow::~MainWindow() { rclcpp::shutdown(); }
 
 void MainWindow::ComputeAndUpdateTF() {
+  // Rotation: Rz * Ry * Rx
   // clang-format off
   tf = Eigen::Translation3d(ui->le_x->text().toDouble(), ui->le_y->text().toDouble(), ui->le_z->text().toDouble()) *
-       Eigen::AngleAxisd(ui->le_rx->text().toDouble() * M_PI / 180., Eigen::Vector3d::UnitX()) *
+       Eigen::AngleAxisd(ui->le_rz->text().toDouble() * M_PI / 180., Eigen::Vector3d::UnitZ()) *
        Eigen::AngleAxisd(ui->le_ry->text().toDouble() * M_PI / 180., Eigen::Vector3d::UnitY()) *
-       Eigen::AngleAxisd(ui->le_rz->text().toDouble() * M_PI / 180., Eigen::Vector3d::UnitZ());
+       Eigen::AngleAxisd(ui->le_rx->text().toDouble() * M_PI / 180., Eigen::Vector3d::UnitX());
   // clang-format on
 
   tf2_msgs::msg::TFMessage tf_msg;
